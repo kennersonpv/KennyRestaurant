17922271f1be4f61b666501bad3ba91e6293b831
kennersonpv <kennersondudu@hotmail.com>
2022-12-14 20:14:47 -0300

    remove test

--- Kenny.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
+++ Kenny.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -4,7 +4,6 @@ namespace Kenny.Services.ProductAPI.DbContexts
 {
     public class ApplicationDbContext : DbContext
     {
-        const int abc = 2;       
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
 
