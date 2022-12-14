9319f278255510a3986aa5e523be04f0cb4d071e
kenne <kenne@DESKTOP-P6UU1CS>
2022-12-14 17:22:27 -0300

    Add project files.

---
+++ Kenny.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Kenny.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
