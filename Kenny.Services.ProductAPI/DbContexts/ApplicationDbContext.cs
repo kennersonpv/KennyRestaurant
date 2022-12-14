11f0df938d7def132491656ee136404aff40d8ec
kennersonpv <kennersonpv@gmail.com>
2022-12-14 20:11:22 -0300

    teste commit user

--- Kenny.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
+++ Kenny.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -4,6 +4,7 @@ namespace Kenny.Services.ProductAPI.DbContexts
 {
     public class ApplicationDbContext : DbContext
     {
+        const int abc = 1;       
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
 
