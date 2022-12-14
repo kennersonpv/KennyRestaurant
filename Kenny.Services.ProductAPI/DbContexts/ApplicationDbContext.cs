a012f01c581920e6333bdd617cf03bf5b67aabc2
kennersonpv <kennersondudu@hotmail.com>
2022-12-14 20:26:40 -0300

    create Product model and migrations

--- Kenny.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
+++ Kenny.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Kenny.Services.ProductAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Kenny.Services.ProductAPI.DbContexts
 {
@@ -6,7 +7,8 @@ namespace Kenny.Services.ProductAPI.DbContexts
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-
         }
+
+        public DbSet<Product> Products { get; set; }
     }
 }
