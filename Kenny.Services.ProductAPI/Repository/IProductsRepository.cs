77bd48106c1a978ddce820362760aa514aa4e607
kennersonpv <kennersondudu@hotmail.com>
2022-12-15 18:36:06 -0300

    create IproductRepository

--- Kenny.Services.ProductAPI/Repository/IProductsRepository.cs
+++ Kenny.Services.ProductAPI/Repository/IProductsRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace Kenny.Services.ProductAPI.Repository
+﻿using Kenny.Services.ProductAPI.Models.Dto;
+
+namespace Kenny.Services.ProductAPI.Repository
 {
     public interface IProductsRepository
     {
+        Task<IEnumerable<ProductDto>> GetProducts();
+        Task<ProductDto> GetProductById(int productId);
+        Task<ProductDto> CreateUpdateProduct(ProductDto product);
+        Task<bool> DeleteProduct(int productId);
     }
 }
