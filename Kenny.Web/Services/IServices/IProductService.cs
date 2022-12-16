fcd84c4cbf35096d5b654c7bb38ca11d5b7df338
kennersonpv <kennersondudu@hotmail.com>
2022-12-16 07:33:25 -0300

    Create product repository interface and dtos in main project

---
+++ Kenny.Web/Services/IServices/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using Kenny.Web.Models.Dto;
+
+namespace Kenny.Web.Services.IServices
+{
+    public interface IProductService
+    {
+        Task<T> GetAllProductsAsync<T>();
+        Task<T> GetProductByIdAsync<T>(int id);
+        Task<T> CreateProductAsync<T>(ProductDto productDto);
+        Task<T> UpdateProductAsync<T>(ProductDto productDto);
+        Task<T> DeleteProductAsync<T>(int id);
+    }
+}
