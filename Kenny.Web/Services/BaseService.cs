d0c4d3b1ce443ba55ef9362aa28eb2683fc13200
kennersonpv <kennersondudu@hotmail.com>
2022-12-16 08:30:53 -0300

    Create api BaseService

---
+++ Kenny.Web/Services/BaseService.cs
@@ -0,0 +1,84 @@
+﻿using Kenny.Web.Models;
+using Kenny.Web.Models.Dto;
+using Kenny.Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Kenny.Web.Services
+{
+    public class BaseService : IBaseService
+    {
+        public ResponseDto responseModel { get; set; }
+        public IHttpClientFactory httpClient { get; set; }
+
+        public BaseService( IHttpClientFactory httpClient)
+        {
+            this.responseModel = new ResponseDto();
+            this.httpClient = httpClient;
+        }
+
+        public async Task<T> SendAsync<T>(ApiRequest apiRequest)
+        {
+            try
+            {
+                var client = httpClient.CreateClient("KennyAPI");
+
+                HttpRequestMessage message = new HttpRequestMessage();
+                //Can be
+                //message.Headers.Add("Content-Type", "application/json");
+                message.Headers.Add("Content-Type", "application/json");
+                message.RequestUri = new Uri(apiRequest.Url);
+
+                if(apiRequest.Data != null)
+                {
+                    message.Content = new StringContent(JsonConvert.SerializeObject(apiRequest.Data), Encoding.UTF8, "application/json");
+                }
+
+                HttpResponseMessage httpResponse = null;
+                switch(apiRequest.ApiType)
+                {
+                    case SD.ApiType.POST:
+                        message.Method = HttpMethod.Post;
+                        break;
+                    case SD.ApiType.PUT:
+                        message.Method = HttpMethod.Put;
+                        break;
+                    case SD.ApiType.DELETE:
+                        message.Method = HttpMethod.Delete;
+                        break;
+                    default:
+                        message.Method = HttpMethod.Get;
+                        break;
+                }
+
+                client.DefaultRequestHeaders.Clear();
+                httpResponse = await client.SendAsync(message);
+
+                var apiContent = await httpResponse.Content.ReadAsStringAsync();
+                var apiResponseDto = JsonConvert.DeserializeObject<T>(apiContent);
+                return apiResponseDto;
+            }
+            catch(Exception ex)
+            {
+                var responseDto = new ResponseDto
+                {
+                    DisplayMessage = "Error",
+                    ErrorMessages = new List<string> { Convert.ToString(ex.Message) },
+                    IsSuccess = false
+                };
+
+                var response = JsonConvert.SerializeObject(responseDto);
+                var apiResponseDto = JsonConvert.DeserializeObject<T>(response);
+                return apiResponseDto;
+
+            }
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ Kenny.Web/Services/IServices/IBaseService.cs
@@ -0,0 +1,12 @@
+﻿using Kenny.Web.Models;
+using Kenny.Web.Models.Dto;
+
+namespace Kenny.Web.Services.IServices
+{
+    public interface IBaseService : IDisposable
+    {
+        public ResponseDto responseModel { get; set; }
+        Task<T> SendAsync<T>(ApiRequest apiRequest);
+
+    }
+}
