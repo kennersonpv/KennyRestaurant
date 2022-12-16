b1dc044e4c32ba083f58c52be34ca8a4f2cd7447
kennersonpv <kennersondudu@hotmail.com>
2022-12-16 07:37:00 -0300

    create ApiRequest

---
+++ Kenny.Web/Models/ApiRequest.cs
@@ -0,0 +1,12 @@
+﻿using static Kenny.Web.SD;
+
+namespace Kenny.Web.Models
+{
+    public class ApiRequest
+    {
+        public ApiType ApiType { get; set; }
+        public string Url { get; set; }
+        public string Data { get; set; }
+        public string AccessToken { get; set; }
+    }
+}
