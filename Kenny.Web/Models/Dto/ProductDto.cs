99f3117d7c3c5755c17c983a04b68f748304bd19
kennersonpv <kennersondudu@hotmail.com>
2023-01-15 08:28:03 -0300

    implement details plage

--- Kenny.Web/Models/Dto/ProductDto.cs
+++ Kenny.Web/Models/Dto/ProductDto.cs
@@ -1,12 +1,21 @@
-﻿namespace Kenny.Web.Models.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Kenny.Web.Models.Dto
 {
     public class ProductDto
     {
+        public ProductDto()
+        {
+            Count = 1;
+        }
+
         public int ProductId { get; set; }
         public string Name { get; set; }
         public double Price { get; set; }
         public string Description { get; set; }
         public string CategoryName { get; set; }
         public string ImageUrl { get; set; }
+        [Range(1,1000)]
+        public int Count { get; set; }
     }
 }
