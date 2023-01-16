3014ae2d19b4b8257a05d8250b15db8a72b81108
kennersonpv <kennersondudu@hotmail.com>
2023-01-16 20:24:39 -0300

    implement CreateUpdateCartAsync method

--- Kenny.Services.ShoppingCartAPI/Repository/CartRepository.cs
+++ Kenny.Services.ShoppingCartAPI/Repository/CartRepository.cs
@@ -4,6 +4,7 @@ using Kenny.Services.ShoppingCartAPI.Models;
 using Kenny.Services.ShoppingCartAPI.Models.Dto;
 using Kenny.Services.ShoppingCartAPI.Repository.Interfaces;
 using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
 
 namespace Kenny.Services.ShoppingCartAPI.Repository
 {
@@ -27,7 +28,7 @@ namespace Kenny.Services.ShoppingCartAPI.Repository
         {
             var cart = _mapper.Map<Cart>(cartDto);
 
-            var productInDb = _db.Products.FirstOrDefault(p => p.ProductId == cartDto.CartDetails.FirstOrDefault().ProductId);
+            var productInDb = await _db.Products.FirstOrDefaultAsync(p => p.ProductId == cartDto.CartDetails.FirstOrDefault().ProductId);
 
             if (productInDb == null)
             {
@@ -35,8 +36,40 @@ namespace Kenny.Services.ShoppingCartAPI.Repository
                 await _db.SaveChangesAsync();
             }
 
+            var cartHeaderFromDb = await _db.CartHeaders.AsNoTracking().FirstOrDefaultAsync(c => c.UserId == cart.CartHeader.UserId);
 
-            throw new NotImplementedException();
+            if(cartHeaderFromDb == null)
+            {
+                _db.CartHeaders.Add(cart.CartHeader);
+                await _db.SaveChangesAsync();
+                cart.CartDetails.FirstOrDefault().CartHeaderId = cart.CartHeader.CartHeaderId;
+                cart.CartDetails.FirstOrDefault().Product = null;
+                _db.CartDetails.Add(cart.CartDetails.FirstOrDefault());
+                await _db.SaveChangesAsync();
+            }
+            else
+            {
+                var cartDetailsFromDb = await _db.CartDetails.AsNoTracking().FirstOrDefaultAsync(
+                    c => c.ProductId == cart.CartDetails.FirstOrDefault().ProductId &&
+                    c.CartHeaderId == cartHeaderFromDb.CartHeaderId);
+
+                if (cartDetailsFromDb == null)
+                {
+                    cart.CartDetails.FirstOrDefault().CartHeaderId = cartDetailsFromDb.CartHeaderId;
+                    cart.CartDetails.FirstOrDefault().Product = null;
+                    _db.CartDetails.Add(cart.CartDetails.FirstOrDefault());
+                    await _db.SaveChangesAsync();
+                }
+                else
+                {
+                    cart.CartDetails.FirstOrDefault().Product = null;
+                    cart.CartDetails.FirstOrDefault().Count += cartDetailsFromDb.Count;
+                    _db.CartDetails.Update(cart.CartDetails.FirstOrDefault());
+                    await _db.SaveChangesAsync();
+                }
+            }
+
+            var cart = _mapper.Map<Cart>(cartDto);
         }
 
         public async Task<CartDto> GetCartByUserIdAsync(string userId)
