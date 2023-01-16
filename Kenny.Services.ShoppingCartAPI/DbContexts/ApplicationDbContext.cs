d66084919ce6963fb5d8ad66898fe8d0129cce1b
kennersonpv <kennersondudu@hotmail.com>
2023-01-16 18:32:23 -0300

    add services on startup

---
+++ Kenny.Services.ShoppingCartAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Kenny.Services.ShoppingCartAPI.DbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        //public DbSet<Product> Products { get; set; }
+    }
+}
