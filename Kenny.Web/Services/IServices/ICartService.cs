9031d69fbe151d8cf6426eddc88e267a7d4545b6
kennersonpv <kennersondudu@hotmail.com>
2023-01-16 22:08:21 -0300

    create ICartService interface

---
+++ Kenny.Web/Services/IServices/ICartService.cs
@@ -0,0 +1,12 @@
+﻿using Kenny.Web.Models.Dto;
+
+namespace Kenny.Web.Services.IServices
+{
+    public interface ICartService
+    {
+        Task<T> GetCartByUserIdAsync<T>(string userId, string token = null);
+        Task<T> AddToCartByUserIdAsync<T>(CartDto cartDto, string token = null);
+        Task<T> UdateCartByUserIdAsync<T>(CartDto cartDto, string token = null);
+        Task<T> RemoveFromCartAsync<T>(int cartId, string token = null);
+    }
+}
