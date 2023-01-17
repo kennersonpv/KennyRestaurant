a85a9b02c06fe745119e579240a8222bf2076bd0
kennersonpv <kennersondudu@hotmail.com>
2023-01-16 22:18:38 -0300

    configure cart service

---
+++ Kenny.Web/Services/CartService.cs
@@ -0,0 +1,60 @@
+﻿using Kenny.Web.Models;
+using Kenny.Web.Models.Dto;
+using Kenny.Web.Services.IServices;
+
+namespace Kenny.Web.Services
+{
+    public class CartService : BaseService, ICartService
+    {
+        private const string API_PATH = "/api/cart/";
+        private readonly IHttpClientFactory _httpClient;
+
+        public CartService(IHttpClientFactory httpClient) : base(httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<T> AddToCartByUserIdAsync<T>(CartDto cartDto, string token = null)
+        {
+            return await this.SendAsync<T>(new ApiRequest()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = cartDto,
+                Url = SD.ShoppingCartAPIBase + API_PATH + "AddCart",
+                AccessToken = token
+            });
+        }
+
+        public async Task<T> GetCartByUserIdAsync<T>(string userId, string token = null)
+        {
+            return await this.SendAsync<T>(new ApiRequest()
+            {
+                ApiType = SD.ApiType.GET,
+                Url = SD.ShoppingCartAPIBase + API_PATH + "GetCart/" + userId,
+                AccessToken = token
+            });
+        }
+
+        public async Task<T> RemoveFromCartAsync<T>(int cartId, string token = null)
+        {
+            return await this.SendAsync<T>(new ApiRequest()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = cartId,
+                Url = SD.ShoppingCartAPIBase + API_PATH + "RemoveCart",
+                AccessToken = token
+            });
+        }
+
+        public async Task<T> UdateCartByUserIdAsync<T>(CartDto cartDto, string token = null)
+        {
+            return await this.SendAsync<T>(new ApiRequest()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = cartDto,
+                Url = SD.ShoppingCartAPIBase + API_PATH + "UpdateCart",
+                AccessToken = token
+            });
+        }
+    }
+}
