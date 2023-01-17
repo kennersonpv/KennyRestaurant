9c54731abbbba3b415b60699982521eb8ea54173
kennersonpv <kennersondudu@hotmail.com>
2023-01-17 17:44:24 -0300

    Configure CouponAPI at startup

---
+++ Kenny.Services.CouponAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Kenny.Services.CouponAPI.DbContexts
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+		}
+	}
+}
--- Kenny.Services.ProductAPI/Program.cs
+++ Kenny.Services.ProductAPI/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-	c.SwaggerDoc("v1", new OpenApiInfo { Title = "Kenny.Services.ShoppingCartAPI", Version = "v1" });
+	c.SwaggerDoc("v1", new OpenApiInfo { Title = "Kenny.Services.ProductAPI", Version = "v1" });
 	c.EnableAnnotations();
 	c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
 	{
