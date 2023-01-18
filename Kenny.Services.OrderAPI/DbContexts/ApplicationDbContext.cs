ef01397b490a4680b3835e6acc9a21ff8fc1b16c
kennersonpv <kennersondudu@hotmail.com>
2023-01-18 14:36:57 -0300

    create models and dbcontext

---
+++ Kenny.Services.OrderAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Kenny.Services.OrderAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kenny.Services.OrderAPI.DbContexts
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+		}
+
+		public DbSet<OrderHeader> OrderHeaders { get; set; }
+		public DbSet<OrderDetails> OrderDetails { get; set; }
+	}
+}
