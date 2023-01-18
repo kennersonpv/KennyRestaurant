2ad94f285ea4d1ef147b41db06dae3726a7b490f
kennersonpv <kennersondudu@hotmail.com>
2023-01-18 15:53:45 -0300

    create receiving messages models

---
+++ Kenny.Services.OrderAPI/Messaging/AzureServiceBusConsumer.cs
@@ -0,0 +1,18 @@
+﻿using Azure.Messaging.ServiceBus;
+using Kenny.Services.OrderAPI.Messages;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Kenny.Services.OrderAPI.Messaging
+{
+	public class AzureServiceBusConsumer
+	{
+		private async Task OnCheckoutMessageReceived (ProcessMessageEventArgs args)
+		{
+			var message = args.Message;
+			var body = Encoding.UTF8.GetString(message.Body);
+
+			var checkoutHeaderDto = JsonConvert.DeserializeObject<CheckoutHeaderDto>(body);
+		}
+	}
+}
