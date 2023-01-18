42c66d2ab76c8547e56b2a404b286e7005ab3f13
kennersonpv <kennersondudu@hotmail.com>
2023-01-18 17:19:50 -0300

    create CouponRepository on ShoppingCartApi

---
+++ Kenny.Services.ShoppingCartAPI/Repository/CouponRepository.cs
@@ -0,0 +1,13 @@
+﻿using Kenny.Services.ShoppingCartAPI.Models.Dto;
+using Kenny.Services.ShoppingCartAPI.Repository.Interfaces;
+
+namespace Kenny.Services.ShoppingCartAPI.Repository
+{
+	public class CouponRepository : ICoupunRepository
+	{
+		public Task<CouponDto> GetCoupon(string name)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Kenny.Services.ShoppingCartAPI/Repository/Interfaces/ICoupunRepository.cs
@@ -0,0 +1,9 @@
+﻿using Kenny.Services.ShoppingCartAPI.Models.Dto;
+
+namespace Kenny.Services.ShoppingCartAPI.Repository.Interfaces
+{
+	public interface ICoupunRepository
+	{
+		Task<CouponDto> GetCoupon(string name);
+	}
+}
