b4f93931aa6ec7f27b1773d172897021f7532fad
kennersonpv <kennersondudu@hotmail.com>
2023-01-18 15:05:40 -0300

    add order tables migration and create orderRepository

---
+++ Kenny.Services.OrderAPI/Repository/IOrderRepository.cs
@@ -0,0 +1,10 @@
+﻿using Kenny.Services.OrderAPI.Models;
+
+namespace Kenny.Services.OrderAPI.Repository
+{
+	public interface IOrderRepository
+	{
+		Task<bool> AddOrderAsync(OrderHeader orderHeader);
+		Task UpdateOrderPaymentStatus(int orderHeaderId, bool paid);
+	}
+}
---
+++ Kenny.Services.OrderAPI/Repository/OrderRepository.cs
@@ -0,0 +1,17 @@
+﻿using Kenny.Services.OrderAPI.Models;
+
+namespace Kenny.Services.OrderAPI.Repository
+{
+	public class OrderRepository : IOrderRepository
+	{
+		public Task<bool> AddOrderAsync(OrderHeader orderHeader)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task UpdateOrderPaymentStatus(int orderHeaderId, bool paid)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
