6687a7534285bbfe5d0298474d343f4c71c51851
kennersonpv <kennersondudu@hotmail.com>
2023-01-18 15:16:05 -0300

    create OrderRepository

--- Kenny.Services.OrderAPI/Repository/OrderRepository.cs
+++ Kenny.Services.OrderAPI/Repository/OrderRepository.cs
@@ -1,17 +1,41 @@
-﻿using Kenny.Services.OrderAPI.Models;
+﻿using Kenny.Services.OrderAPI.DbContexts;
+using Kenny.Services.OrderAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Kenny.Services.OrderAPI.Repository
 {
 	public class OrderRepository : IOrderRepository
 	{
-		public Task<bool> AddOrderAsync(OrderHeader orderHeader)
+		private readonly DbContextOptions<ApplicationDbContext> _dbContext;
+		public OrderRepository(DbContextOptions<ApplicationDbContext> dbContext)
 		{
-			throw new NotImplementedException();
+			_dbContext = dbContext;
 		}
 
-		public Task UpdateOrderPaymentStatus(int orderHeaderId, bool paid)
+		public async Task<bool> AddOrderAsync(OrderHeader orderHeader)
 		{
-			throw new NotImplementedException();
+			try
+			{
+				await using var _db = new ApplicationDbContext(_dbContext);
+				_db.OrderHeaders.Add(orderHeader);
+				await _db.SaveChangesAsync();
+				return true;
+			}
+			catch
+			{
+				return false;
+			}
+		}
+
+		public async Task UpdateOrderPaymentStatus(int orderHeaderId, bool paid)
+		{
+			await using var _db = new ApplicationDbContext(_dbContext);
+			var orderHeaderFromDb = await _db.OrderHeaders.FirstOrDefaultAsync(u => u.OrderHeaderId == orderHeaderId);
+			if(orderHeaderFromDb != null)
+			{
+				orderHeaderFromDb.PaymentStatus = paid;
+				await _db.SaveChangesAsync();
+			}
 		}
 	}
 }
