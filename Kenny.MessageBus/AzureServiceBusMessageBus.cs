a3fa534d4fd72d9fa0ebdcfff7de484b73298535
kennersonpv <kennersondudu@hotmail.com>
2023-01-18 11:23:21 -0300

    create messagebus methods

---
+++ Kenny.MessageBus/AzureServiceBusMessageBus.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.ServiceBus.Core;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kenny.MessageBus
+{
+	public class AzureServiceBusMessageBus : IMessageBus
+	{
+		private string connectionString = "Endpoint=sb://kennyrestaurant.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=lq5Rg1a91f1qavtou8mrbCTlGfhcyRRNMQ4SYhOc4MQ=";
+
+		public async Task PublicMessage(BaseMessage message, string topicName)
+		{
+			ISenderClient senderClient = new TopicClient(connectionString, topicName);
+
+			var jsonMessage = JsonConvert.SerializeObject(message);
+			var finalMessage = new Message(Encoding.UTF8.GetBytes(jsonMessage))
+			{ 
+				CorrelationId = Guid.NewGuid().ToString()
+			};
+			await senderClient.SendAsync(finalMessage);
+			await senderClient.CloseAsync();
+		}
+	}
+}
---
+++ Kenny.MessageBus/IMessageBus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kenny.MessageBus
+{
+	internal interface IMessageBus
+	{
+		Task PublicMessage(BaseMessage message, string topicName);
+	}
+}
