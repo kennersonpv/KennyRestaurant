cce273797797322e924ae7fb6c5c8ef6d9d23543
kennersonpv <kennersondudu@hotmail.com>
2023-01-27 18:30:28 -0300

    Add authentication and ocelot

--- Kenny.Gateway/Program.cs
+++ Kenny.Gateway/Program.cs
@@ -1,5 +1,24 @@
+using Microsoft.IdentityModel.Tokens;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+// Add Authentication service
+builder.Services.AddAuthentication("Bearer")
+	.AddJwtBearer("Bearer", options =>
+	{
+		options.Authority = builder.Configuration["ServiceUrls:IdentityAPI"];
+		options.TokenValidationParameters = new TokenValidationParameters
+		{
+			ValidateAudience = false
+		};
+	});
+
+builder.Services.AddOcelot();
+
+await app.UseOcelot();
 
 app.MapGet("/", () => "Hello World!");
 
