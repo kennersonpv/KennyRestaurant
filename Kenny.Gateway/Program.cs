919ca7e5113e1571815a09bd3a10d36fcb8f89e5
kennersonpv <kennersondudu@hotmail.com>
2023-01-28 08:34:03 -0300

    create routes for product api endpoints

--- Kenny.Gateway/Program.cs
+++ Kenny.Gateway/Program.cs
@@ -3,20 +3,31 @@ using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
 // Add Authentication service
 builder.Services.AddAuthentication("Bearer")
 	.AddJwtBearer("Bearer", options =>
 	{
-		options.Authority = builder.Configuration["ServiceUrls:IdentityAPI"];
+		options.Authority = "https://localhost:7009";
 		options.TokenValidationParameters = new TokenValidationParameters
 		{
 			ValidateAudience = false
 		};
 	});
 
+// Add Authorization
+builder.Services.AddAuthorization(options =>
+{
+	options.AddPolicy("ApiScope", policy =>
+	{
+		policy.RequireAuthenticatedUser();
+		policy.RequireClaim("scope", "kenny");
+	});
+});
+
 builder.Services.AddOcelot();
+
+var app = builder.Build();
 
 await app.UseOcelot();
 
--- Kenny.Web/Services/ProductService.cs
+++ Kenny.Web/Services/ProductService.cs
@@ -4,65 +4,72 @@ using Kenny.Web.Services.IServices;
 
 namespace Kenny.Web.Services
 {
-    public class ProductService : BaseService, IProductService
-    {
-        private const string API_PATH = "/api/products/";
-        private readonly IHttpClientFactory _clientFactory;
-        public ProductService(IHttpClientFactory clientFactory) : base(clientFactory)
-        {
-            _clientFactory = clientFactory;
-        }
+	public class ProductService : BaseService, IProductService
+	{
+		private const string API_PATH = "/api/products/";
+		private readonly IHttpClientFactory _clientFactory;
+		public ProductService(IHttpClientFactory clientFactory) : base(clientFactory)
+		{
+			_clientFactory = clientFactory;
+		}
 
-        public async Task<T> CreateProductAsync<T>(ProductDto productDto, string token)
-        {
-            return await this.SendAsync<T>(new ApiRequest()
-            {
-                ApiType = SD.ApiType.POST,
-                Data = productDto,
-                Url = SD.ProductAPIBase + API_PATH,
-                AccessToken = token
-            });
-        }
+		public async Task<T> CreateProductAsync<T>(ProductDto productDto, string token)
+		{
+			return await this.SendAsync<T>(new ApiRequest()
+			{
+				ApiType = SD.ApiType.POST,
+				Data = productDto,
+				Url = SD.ProductAPIBase + API_PATH,
+				AccessToken = token
+			});
+		}
 
-        public async Task<T> DeleteProductAsync<T>(int id, string token)
-        {
-            return await this.SendAsync<T>(new ApiRequest()
-            {
-                ApiType = SD.ApiType.DELETE,
-                Url = SD.ProductAPIBase + API_PATH + id,
-                AccessToken = token
-            });
-        }
+		public async Task<T> DeleteProductAsync<T>(int id, string token)
+		{
+			return await this.SendAsync<T>(new ApiRequest()
+			{
+				ApiType = SD.ApiType.DELETE,
+				Url = SD.ProductAPIBase + API_PATH + id,
+				AccessToken = token
+			});
+		}
 
-        public async Task<T> GetAllProductsAsync<T>(string token)
-        {
-            return await this.SendAsync<T>(new ApiRequest()
-            {
-                ApiType = SD.ApiType.GET,
-                Url = SD.ProductAPIBase + API_PATH,
-                AccessToken = token
-            });
-        }
+		public async Task<T> GetAllProductsAsync<T>(string token)
+		{
+			try
+			{
+				return await this.SendAsync<T>(new ApiRequest()
+				{
+					ApiType = SD.ApiType.GET,
+					Url = SD.ProductAPIBase + API_PATH,
+					AccessToken = token
+				});
+			}
+			catch (Exception e)
+			{
+				throw;
+			}
+		}
 
-        public async Task<T> GetProductByIdAsync<T>(int id, string token)
-        {
-            return await this.SendAsync<T>(new ApiRequest()
-            {
-                ApiType = SD.ApiType.GET,
-                Url = SD.ProductAPIBase + API_PATH + id,
-                AccessToken = token
-            });
-        }
+		public async Task<T> GetProductByIdAsync<T>(int id, string token)
+		{
+			return await this.SendAsync<T>(new ApiRequest()
+			{
+				ApiType = SD.ApiType.GET,
+				Url = SD.ProductAPIBase + API_PATH + id,
+				AccessToken = token
+			});
+		}
 
-        public async Task<T> UpdateProductAsync<T>(ProductDto productDto, string token)
-        {
-            return await this.SendAsync<T>(new ApiRequest()
-            {
-                ApiType = SD.ApiType.PUT,
-                Data = productDto,
-                Url = SD.ProductAPIBase + API_PATH,
-                AccessToken = token
-            });
-        }
-    }
+		public async Task<T> UpdateProductAsync<T>(ProductDto productDto, string token)
+		{
+			return await this.SendAsync<T>(new ApiRequest()
+			{
+				ApiType = SD.ApiType.PUT,
+				Data = productDto,
+				Url = SD.ProductAPIBase + API_PATH,
+				AccessToken = token
+			});
+		}
+	}
 }
