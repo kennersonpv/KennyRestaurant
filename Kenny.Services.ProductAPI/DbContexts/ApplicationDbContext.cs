9e3211674ad6fc0df94d5f1bde7675b643736b48
kennersonpv <kennersondudu@hotmail.com>
2023-02-07 18:42:57 -0300

    update product appDBContext

--- Kenny.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
+++ Kenny.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -22,7 +22,7 @@ namespace Kenny.Services.ProductAPI.DbContexts
                 Name = "Feijoada",
                 Price = 15,
                 Description = "Black beans with meat and rice",
-                ImageUrl = "",
+                ImageUrl = "https://assets.unileversolutions.com/recipes-v2/54349.jpg",
                 CategoryName = "Meal"
             });
             modelBuilder.Entity<Product>().HasData(new Product
@@ -31,7 +31,7 @@ namespace Kenny.Services.ProductAPI.DbContexts
                 Name = "Cheesburguer",
                 Price = 29.99,
                 Description = "It's our best seller. Picanha meat with cheddar cheese and a lot of bacon",
-                ImageUrl = "",
+                ImageUrl = "https://s2.glbimg.com/jJirZVMNK5ZsZ9UDLKQBqPu3iXk=/620x455/e.glbimg.com/og/ed/f/original/2020/10/20/hamburgueria_bob_beef_-_dia_das_criancas_-_foto_pfz_studio__norma_lima.jpg",
                 CategoryName = "Sandwich"
             });
             modelBuilder.Entity<Product>().HasData(new Product
@@ -40,7 +40,7 @@ namespace Kenny.Services.ProductAPI.DbContexts
                 Name = "Brigadeiro",
                 Price = 10.99,
                 Description = "Chocolate balls with grainy chocolate",
-                ImageUrl = "",
+                ImageUrl = "https://img.itdg.com.br/tdg/images/recipes/000/000/114/201447/201447_original.jpg",
                 CategoryName = "Dessert"
             });
             modelBuilder.Entity<Product>().HasData(new Product
@@ -49,7 +49,7 @@ namespace Kenny.Services.ProductAPI.DbContexts
                 Name = "Picanha",
                 Price = 45,
                 Description = "Juicy meet with rice and fries",
-                ImageUrl = "",
+                ImageUrl = "https://marettimo.com.br/blog/wp-content/uploads/2022/10/Como-identificar-a-carne-que-parece-picanha.jpg",
                 CategoryName = "Meal"
             });
         }
