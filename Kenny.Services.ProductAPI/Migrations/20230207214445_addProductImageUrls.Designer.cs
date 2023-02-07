39880bb94832f656b9c48872d67a8ef1291c4f6a
kennersonpv <kennersondudu@hotmail.com>
2023-02-07 18:47:23 -0300

    update migrations

--- Kenny.Services.ProductAPI/Migrations/20230207214445_addProductImageUrls.Designer.cs
+++ Kenny.Services.ProductAPI/Migrations/20230207214445_addProductImageUrls.Designer.cs
@@ -3,6 +3,7 @@ using Kenny.Services.ProductAPI.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Kenny.Services.ProductAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230207214445_addProductImageUrls")]
+    partial class addProductImageUrls
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Kenny.Services.ProductAPI/Migrations/20230207214445_addProductImageUrls.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kenny.Services.ProductAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class addProductImageUrls : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 1,
+                column: "ImageUrl",
+                value: "https://assets.unileversolutions.com/recipes-v2/54349.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 2,
+                column: "ImageUrl",
+                value: "https://s2.glbimg.com/jJirZVMNK5ZsZ9UDLKQBqPu3iXk=/620x455/e.glbimg.com/og/ed/f/original/2020/10/20/hamburgueria_bob_beef_-_dia_das_criancas_-_foto_pfz_studio__norma_lima.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 3,
+                column: "ImageUrl",
+                value: "https://img.itdg.com.br/tdg/images/recipes/000/000/114/201447/201447_original.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 4,
+                column: "ImageUrl",
+                value: "https://marettimo.com.br/blog/wp-content/uploads/2022/10/Como-identificar-a-carne-que-parece-picanha.jpg");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 1,
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 2,
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 3,
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 4,
+                column: "ImageUrl",
+                value: "");
+        }
+    }
+}
